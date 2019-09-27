34fdf519ae2dbb02932c4c0f568ddadd0a66183f
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2019-09-27 11:32:50 +0200

    control test

--- src/test/java/it/kamaladafrica/codicefiscale/internal/ControlPartTest.java
+++ src/test/java/it/kamaladafrica/codicefiscale/internal/ControlPartTest.java
@@ -1,34 +1,49 @@
 package it.kamaladafrica.codicefiscale.internal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class ControlPartTest {
 
-	@Test
-	public void testFromString() {
-		fail("Not yet implemented");
-	}
+	private final String CODICE_FISCALE_UNCHECKED = "RSSMRA75C22H501";
+	private final String CONTROL = "I";
+
+	private final String CODICE_FISCALE_UNCHECKED_2 = "RSSMRA75C22H5LM";
+	private final String CONTROL_2 = "L";
 
 	@Test
 	public void testOf() {
-		fail("Not yet implemented");
+		assertEquals(ControlPart.of(CODICE_FISCALE_UNCHECKED).getValue(), CONTROL);
+		assertEquals(ControlPart.of(CODICE_FISCALE_UNCHECKED_2).getValue(), CONTROL_2);
 	}
 
 	@Test
-	public void testToOmocodeLevel() {
-		fail("Not yet implemented");
+	public void testIsEqualChar() {
+		assertTrue(ControlPart.of(CODICE_FISCALE_UNCHECKED).isEqual(CONTROL.charAt(0)));
+		assertTrue(ControlPart.of(CODICE_FISCALE_UNCHECKED_2).isEqual(CONTROL_2.charAt(0)));
 	}
 
 	@Test
-	public void testGetCity() {
-		fail("Not yet implemented");
+	public void testIsEqualString() {
+		assertTrue(ControlPart.of(CODICE_FISCALE_UNCHECKED).isEqual(CONTROL));
+		assertTrue(ControlPart.of(CODICE_FISCALE_UNCHECKED_2).isEqual(CONTROL_2));
 	}
 
 	@Test
-	public void testGetValue() {
-		fail("Not yet implemented");
+	public void testIsEqualStringBadInput() {
+		try {
+			ControlPart.of(CODICE_FISCALE_UNCHECKED).isEqual("5");
+			fail("IllegalArgumentException was expected to be thrown");
+		} catch (IllegalArgumentException e) {
+		}
+		try {
+			ControlPart.of(CODICE_FISCALE_UNCHECKED).isEqual("AA");
+			fail("IllegalArgumentException was expected to be thrown");
+		} catch (IllegalArgumentException e) {
+		}
 	}
 
 }
