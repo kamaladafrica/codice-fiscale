eb954368ba9a706cf498f09d8c0b0bce1a335640
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2019-09-30 15:38:51 +0200

    workaround lombok

--- src/main/java/it/kamaladafrica/codicefiscale/utils/PartUtils.java
+++ src/main/java/it/kamaladafrica/codicefiscale/utils/PartUtils.java
@@ -1,9 +1,5 @@
 package it.kamaladafrica.codicefiscale.utils;
 
-import static it.kamaladafrica.codicefiscale.utils.RegexUtils.CF_ALLOWED_CHARS;
-import static it.kamaladafrica.codicefiscale.utils.RegexUtils.CONSONANT_PATTERN;
-import static it.kamaladafrica.codicefiscale.utils.RegexUtils.VOWEL_PATTERN;
-import static it.kamaladafrica.codicefiscale.utils.RegexUtils.extract;
 import static org.apache.commons.lang3.StringUtils.defaultString;
 
 import lombok.NonNull;
@@ -15,15 +11,15 @@ public class PartUtils {
 	public final static String EMPTY_STRING = "";
 
 	public static String normalizeString(String s) {
-		return extract(CF_ALLOWED_CHARS, defaultString(s).toUpperCase());
+		return RegexUtils.extract(RegexUtils.CF_ALLOWED_CHARS, defaultString(s).toUpperCase());
 	}
 
 	public static String extractConsonants(@NonNull String s) {
-		return extract(CONSONANT_PATTERN, s);
+		return RegexUtils.extract(RegexUtils.CONSONANT_PATTERN, s);
 	}
 
 	public static String extractVowels(@NonNull String s) {
-		return extract(VOWEL_PATTERN, s);
+		return RegexUtils.extract(RegexUtils.VOWEL_PATTERN, s);
 	}
 
 }
