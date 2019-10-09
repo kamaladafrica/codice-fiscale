14f796ef6d88f778726d986546ca801f523ef68a
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2019-10-09 16:34:43 +0200

    omocode utils lombok

--- src/main/java/it/kamaladafrica/codicefiscale/utils/OmocodeUtils.java
+++ src/main/java/it/kamaladafrica/codicefiscale/utils/OmocodeUtils.java
@@ -2,6 +2,9 @@ package it.kamaladafrica.codicefiscale.utils;
 
 import com.google.common.primitives.ImmutableIntArray;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class OmocodeUtils {
 
 	private static final ImmutableIntArray OMOCODE_REPLACE_CHARS = ImmutableIntArray.of('L', 'M', 'N', 'P', 'Q', 'R',
@@ -16,7 +19,7 @@ public class OmocodeUtils {
 		return digit;
 	}
 
-	public static char toDigit(char omocodeChar) {
+	public char toDigit(char omocodeChar) {
 		int index = OMOCODE_REPLACE_CHARS.indexOf(omocodeChar);
 		if (index != -1) {
 			return Character.forDigit(index, 10);
@@ -24,15 +27,15 @@ public class OmocodeUtils {
 		return omocodeChar;
 	}
 
-	public static int level(String value, int[] indexes) {
+	public int level(String value, int[] indexes) {
 		int i = 0;
-		while(i < indexes.length && !Character.isDigit(value.charAt(indexes[i]))) {
+		while (i < indexes.length && !Character.isDigit(value.charAt(indexes[i]))) {
 			i++;
 		}
 		return i;
 	}
 
-	public static String normalize(String value, int[] indexes) {
+	public String normalize(String value, int[] indexes) {
 		StringBuilder sb = new StringBuilder(value);
 		for (int i : indexes) {
 			char c = value.charAt(i);
@@ -41,15 +44,15 @@ public class OmocodeUtils {
 		return sb.toString();
 	}
 
-	public static String normalize(String value) {
+	public String normalize(String value) {
 		return normalize(value, OMOCODE_INDEXES.toArray());
 	}
 
-	public static int level(String value) {
+	public int level(String value) {
 		return level(value, OMOCODE_INDEXES.toArray());
 	}
 
-	public static String apply(String value, int[] indexes) {
+	public String apply(String value, int[] indexes) {
 		StringBuilder sb = new StringBuilder(value);
 		for (int i : indexes) {
 			char c = value.charAt(i);
