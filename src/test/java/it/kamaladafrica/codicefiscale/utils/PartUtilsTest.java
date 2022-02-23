caa94352dc38f144ea3aa6cedbd078c18fe69d39
Alessio D'Innocenti <a.dinnocenti@jdk.it>
2022-02-23 16:23:41 +0100

    fix 2 letters names and surnames

--- src/test/java/it/kamaladafrica/codicefiscale/utils/PartUtilsTest.java
+++ src/test/java/it/kamaladafrica/codicefiscale/utils/PartUtilsTest.java
@@ -1,6 +1,8 @@
 package it.kamaladafrica.codicefiscale.utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,17 +11,32 @@ public class PartUtilsTest {
 	@Test
 	public void testNormalizeString() {
 		assertEquals("A14B", PartUtils.normalizeString("A1,.4B"));
-		assertEquals("", PartUtils.normalizeString(null));
+		assertEquals(PartUtils.EMPTY_STRING, PartUtils.normalizeString(null));
 	}
 
 	@Test
 	public void testExtractConsonants() {
 		assertEquals("B", PartUtils.extractConsonants("A1,.4B"));
+		assertEquals(PartUtils.EMPTY_STRING, PartUtils.extractConsonants("AEIOU"));
 	}
 
 	@Test
 	public void testExtractVowels() {
 		assertEquals("A", PartUtils.extractVowels("A1,.4B"));
+		assertEquals(PartUtils.EMPTY_STRING, PartUtils.extractVowels("BCDF"));
+	}
+
+	@Test
+	public void testHasVowels() {
+		assertTrue(PartUtils.hasVowels("ABC"));
+		assertFalse(PartUtils.hasVowels("BCD"));
+	}
+
+	@Test
+	public void testRemovePlaceholderIfPresent() {
+		assertEquals("BXA", PartUtils.removePlaceholderIfPresent("BXA"));
+		assertEquals("BO", PartUtils.removePlaceholderIfPresent("BOX"));
+		assertEquals("BXX", PartUtils.removePlaceholderIfPresent("BXX"));
 	}
 
 }
