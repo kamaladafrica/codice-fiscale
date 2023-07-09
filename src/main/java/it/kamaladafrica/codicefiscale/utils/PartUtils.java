db9ac4b1b2f733005605f2f8b9e331a4fead9e79
str0el <138615224+str0el@users.noreply.github.com>
2023-07-09 18:42:25 +0200

    added diacritics transliteration

--- src/main/java/it/kamaladafrica/codicefiscale/utils/PartUtils.java
+++ src/main/java/it/kamaladafrica/codicefiscale/utils/PartUtils.java
@@ -14,8 +14,34 @@ public class PartUtils {
 	public static final String EMPTY_STRING = "";
 	public static final String PLACEHOLDER = "X";
 
+	public static final String[] DIACRITICS = new String[] {
+			"À", "Á", "Â", "Ä", "Æ",
+			"Ç", "Č",
+			"È", "É", "Ê", "Ë",
+			"Ì", "Í", "Î", "Ï",
+			"Ò", "Ó", "Ô", "Ö", "Œ",
+			"Ù", "Ú", "Û", "Ü",
+			"Š",
+			"Ž",
+			"ß", "ẞ"
+	};
+
+	public static final String[] DIACRITICS_TRANSLITERATION = new String[] { //
+			"A", "A", "A", "AE", "AE",
+			"C", "C",
+			"E", "E", "E", "E",
+			"I", "I", "I", "I",
+			"O", "O", "O", "OE", "OE",
+			"U", "U", "U", "UE",
+			"S",
+			"Z",
+			"SS", "SS"
+	};
+
 	public static String normalizeString(String s) {
-		return RegexUtils.extract(RegexUtils.CF_ALLOWED_CHARS, defaultString(s).toUpperCase(CodiceFiscale.LOCALE));
+		return RegexUtils.extract(RegexUtils.CF_ALLOWED_CHARS,
+				StringUtils.replaceEach(defaultString(s).toUpperCase(CodiceFiscale.LOCALE),DIACRITICS, DIACRITICS_TRANSLITERATION));
+
 	}
 
 	public static String extractConsonants(String s) {
